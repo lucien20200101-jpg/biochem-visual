4ab027795d5b9f6673557af02810112cf7240217
lucien20200101-jpg <lucien20200101@gmail.com>
2026-01-21 18:31:22 +0800

    Use real Vite React scaffold

--- packages/react-dom/index.js
+++
@@ -1,3 +0,0 @@
-export function render() {
-  throw new Error("react-dom render is not implemented in this minimal scaffold");
-}
--- packages/react/index.js
+++
@@ -1,5 +0,0 @@
-export function createElement(tag, props, ...children) {
-  return { tag, props: props ?? {}, children };
-}
-
-export const Fragment = Symbol("Fragment");
